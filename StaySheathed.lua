f08702717aa6ce59f6b7780e88f6d0ed09fc7072
keyboardturner <20036792+keyboardturner@users.noreply.github.com>
2023-07-09 10:53:44 -0700

    version bump to 10.1.5

--- StaySheathed.lua
+++ StaySheathed.lua
@@ -4,6 +4,12 @@ sheathState:RegisterEvent("UNIT_MODEL_CHANGED")
 sheathState:RegisterEvent("UNIT_TARGET")
 
 local function doSheath()
+	--[[ --this will be for later, rework planned soon(tm)
+	local  class1, class2, class3 = UnitClass("player")
+	if class2 == "MONK" then
+		return
+	end
+	]]
 	if (GetSheathState() == 2) then
 		ToggleSheath()
 	end
